2fb95e6786ab1cf0ba6a43466661ea4f8526f423
sheepfly <happeople@163.com>
2021-08-14 16:56:08 +0800

    提取SORT_METHOD到单独的类中

--- src/main/java/com/sheepfly/common/utils/FileUtil.java
+++ src/main/java/com/sheepfly/common/utils/FileUtil.java
@@ -45,21 +45,4 @@ public class FileUtil {
         return dirList.toArray(arr);
     }
 
-    /**
-     * 列出文件夹内容时的排序方法。
-     *
-     * <p>若按文件名排序，则文件夹总是排在文件前面</p>
-     *
-     * @author sheepfly
-     */
-    public enum SORT_METHOD {
-        NAME("NAME", 1), TYPE("TYPE", 2), SIZE("SIZE", 3);
-        private String key;
-        private int keyNum;
-
-        SORT_METHOD(String key, int keyNum) {
-            this.key = key;
-            this.keyNum = keyNum;
-        }
-    }
 }
